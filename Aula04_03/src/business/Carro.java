d92bee1a46718fe194cbc4ffb859811100f6c15b
Marco Silva <167210845+marantt@users.noreply.github.com>
2026-03-17 21:08:45 -0300

    Implement Carro class extending Veiculo

--- Aula04_03/src/business/Carro.java
+++ Aula04_03/src/business/Carro.java
@@ -1,1 +1,38 @@
+package business;
+
+public class Carro extends Veiculo {
+
+	private int quantidadePortas;
+
+	public Carro(String modelo, String fabricante, int ano) {
+		super(modelo, fabricante, ano);
+	}
+	
+	public Carro() {
+		super();
+	}
+	
+	@Override
+	public void acelerar() {
+		velocidadeAtual += 10;
+		
+		if (this.velocidadeAtual >= 180) {
+			this.velocidadeAtual = 180;
+		}
+		System.out.println("Acelerando o carro. Velocidade atual " + this.velocidadeAtual);
+	}
+
+	@Override
+	public void desacelerar() {
+		// TODO Auto-generated method stub
+		this.velocidadeAtual -= 10;
+		
+		if (this.velocidadeAtual <= 0) {
+			this.velocidadeAtual = 0;
+		}
+		System.out.println("Desacelerando o carro. Velocidade atual " + this.velocidadeAtual);
+	}
+	
+	
+}
 
