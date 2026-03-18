e7a511f90e628f51fa92a5c2006de53d2f3f21c5
Marco Silva <167210845+marantt@users.noreply.github.com>
2026-03-17 21:09:28 -0300

    Add abstract Veiculo class with basic properties

--- Aula04_03/src/business/Veiculo.java
+++ Aula04_03/src/business/Veiculo.java
@@ -1,1 +1,50 @@
+package business;
 
+public abstract class Veiculo {
+
+	protected String modelo;
+	protected String fabricante;
+	protected int ano;
+	protected float velocidadeAtual;
+	
+	public Veiculo(String modelo, String fabricante, int ano) {
+		this.modelo = modelo;
+		this.fabricante = fabricante;
+		this.ano = ano;
+	}
+	
+	public Veiculo() {}
+	
+	public void acelerar() { 
+		velocidadeAtual += 5;
+		if (velocidadeAtual > 180) {
+			velocidadeAtual = 180;
+		}
+	
+	}
+	public void desacelerar() { 
+		velocidadeAtual -= 5; 
+		if (velocidadeAtual < 0) {
+			velocidadeAtual = 0;
+		}
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void parar() { velocidadeAtual = 0;}
+	
+	@Override
+	public String toString() {
+		return String.format("Fabricante: %s, Ano: %d, Modelo: %s", fabricante, ano, modelo);
+	}
+}
