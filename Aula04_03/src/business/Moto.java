361fd77da7517fae216302b0f30a8ef491403128
Marco Silva <167210845+marantt@users.noreply.github.com>
2026-03-17 21:09:06 -0300

    Implement Moto class extending Veiculo

--- Aula04_03/src/business/Moto.java
+++ Aula04_03/src/business/Moto.java
@@ -1,1 +1,37 @@
+package business;
+
+public class Moto extends Veiculo {
+
+	private boolean descansoAcionado;
+	
+	public Moto(String modelo, String fabricante, int ano) {
+		super(modelo, fabricante, ano);
+	}
+	
+	public Moto() {
+		super();
+	}
+	
+	@Override
+	public void acelerar() {
+		super.acelerar();
+		System.out.println("Acelerando a moto. Velocidade atual " + this.velocidadeAtual);
+		
+		this.descansoAcionado = false;
+	}
+	
+	@Override
+	public void desacelerar() {
+		
+		this.velocidadeAtual -= 5;
+		
+		if (velocidadeAtual <= 0) {
+			descansoAcionado = true;
+			this.velocidadeAtual = 0;
+			this.descansoAcionado = true;
+		}
+		
+		System.out.println("Desacelerando a moto. Velocidade atual " + this.velocidadeAtual);
+	}
+}
 
